de79b1a2549ce89325205938fed7dd27966b3668
Charly1j <146923633+Charly1j@users.noreply.github.com>
2024-05-29 23:40:05 -0400

    Add files via upload

---
+++ JavaInWeb_SimpleProject/src/main/java/com/codingdojo/web/Home.java
@@ -0,0 +1,52 @@
+package com.codingdojo.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		String lenguaje = request.getParameter("lenguaje");
+		String ciudad = request.getParameter("ciudad");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Bienvenido, " + nombre +apellido+ "</h1>");
+        out.write("<br></br>");
+        out.write("<h2>Tu lenguaje favorito es: "+ lenguaje+"</h2>");
+        out.write("<h2>Tu ciudad es: "+ ciudad+"</h2>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
